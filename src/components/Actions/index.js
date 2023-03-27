b584a23afe5c9aaa5e5fcc0cb60e28a65b4d3cb2
Nathã Wolff <natha.wolff9@gmail.com>
2023-03-27 02:51:06 +0000

    first part of project has been completed

--- src/components/Actions/index.js
+++ src/components/Actions/index.js
@@ -4,14 +4,42 @@ import {AntDesign} from '@expo/vector-icons'
 
 export default function Actions(){
     return(
-        <ScrollView style={styles.container} horizontal={true}>
+        <ScrollView style={styles.container} horizontal={true} showsHorizontalScrollIndicator={false}>
             <TouchableOpacity style={styles.actionButton}>
                 <View style={styles.areaButton}>
                     <AntDesign name='addfolder' size={26} color="#000"/>
                 </View>
-
                 <Text style={styles.labelButton}>Entrada</Text>
             </TouchableOpacity>
+
+            <TouchableOpacity style={styles.actionButton}>
+                <View style={styles.areaButton}>
+                    <AntDesign name='tagso' size={26} color="#000"/>
+                </View>
+                <Text style={styles.labelButton}>Compras</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.actionButton}>
+                <View style={styles.areaButton}>
+                    <AntDesign name='barcode' size={26} color="#000"/>
+                </View>
+                <Text style={styles.labelButton}>Boletos</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.actionButton}>
+                <View style={styles.areaButton}>
+                    <AntDesign name='setting' size={26} color="#000"/>
+                </View>
+                <Text style={styles.labelButton}>Contas</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.actionButton}>
+                <View style={styles.areaButton}>
+                    <AntDesign name='addfolder' size={26} color="#000"/>
+                </View>
+                <Text style={styles.labelButton}>Entrada</Text>
+            </TouchableOpacity>
+
         </ScrollView>
         
     );
@@ -19,6 +47,30 @@ export default function Actions(){
 
 const styles = StyleSheet.create({
     container:{
-        
+        maxHeight: 84,
+        marginBottom: 14,
+        marginTop: 18,
+        paddingEnd: 14,
+        paddingStart: 14,
+    },
+
+    actionButton:{
+        alignItems: 'center',
+        marginRight: 32,
+    },
+
+    areaButton:{
+        backgroundColor: '#dada',
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    labelButton:{
+        fontWeight: 'bold',
+        marginTop: 4,
+        textAlign: 'center',
     }
 })
